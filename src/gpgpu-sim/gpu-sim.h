7ce9b801a045192e592b31cf1c3c8a5ea205fa91
FarmerJooe <70464752@qq.com>
2025-04-23 13:41:03 +0800

    mee_ecc v1.0

--- src/gpgpu-sim/gpu-sim.h
+++ src/gpgpu-sim/gpu-sim.h
@@ -266,6 +266,7 @@ class memory_config {
     m_address_mapping.init(m_n_mem, m_n_sub_partition_per_memory_channel);
     m_L2_config.init(&m_address_mapping);
     m_META_config.init(&m_address_mapping);
+    m_CTR_config.init(&m_address_mapping);
 
     m_valid = true;
 
